89a7e99eb26eff4bcac517333b4ff4ddd5067619
lostmagicK <35366477+lostmagicK@users.noreply.github.com>
2018-01-12 16:35:41 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dependency2.WebApiApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Dependency2
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            SqlDependency.Start(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+        }
+        protected void Application_End()
+        {
+            //Free the dependency
+            SqlDependency.Stop(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+        }
+    }
+}
